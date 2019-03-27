48cff6a01d0351d8c2949c9fef79089fcbe1e4d5
Filip <filipf6@gmail.com>
2019-03-27 15:55:50 +0100

    frontend skeleton, frontend-backend integration

--- src/main/java/com/pk/fpl_point_predictor/controller/PlayerSamplesController.java
+++ src/main/java/com/pk/fpl_point_predictor/controller/PlayerSamplesController.java
@@ -1,5 +1,7 @@
 package com.pk.fpl_point_predictor.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.pk.fpl_point_predictor.logic.PlayerSamplesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,24 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/player-samples")
 public class PlayerSamplesController {
 
-    private PlayerSamplesService dataCollectorService;
+    private PlayerSamplesService playerSamplesService;
 
     @Autowired
-    public PlayerSamplesController(PlayerSamplesService dataCollectorService) {
-        this.dataCollectorService = dataCollectorService;
+    public PlayerSamplesController(PlayerSamplesService playerSamplesService) {
+        this.playerSamplesService = playerSamplesService;
     }
 
     @RequestMapping(value = "/current-gameweek-stats", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> collectCurrentGameweekStats() {
-        this.dataCollectorService.collectCurrentGameweekPlayerSamplesStats();
+        this.playerSamplesService.collectCurrentGameweekPlayerSamplesStats();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(value = "/last-gameweek-scores", method = RequestMethod.PATCH, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> collectLastGameweekScores() {
-        this.dataCollectorService.collectLastGameweekPlayerSamplesScores();
+        this.playerSamplesService.collectLastGameweekPlayerSamplesScores();
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/test-url", method = RequestMethod.GET)
+    public ResponseEntity<?> testMethod() {
+        System.out.println("test Succesful!");
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode playerSample = mapper.createObjectNode();
+        playerSample.put("id", 1);
+        playerSample.put("name", "Filip Gurgul");
+        System.out.println(playerSample.toString());
+        return new ResponseEntity<>(playerSample.toString(), HttpStatus.OK);
     }
 }
