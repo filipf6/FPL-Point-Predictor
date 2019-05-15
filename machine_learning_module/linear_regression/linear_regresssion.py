6274adc64032f5e5e199737c6005071253ed579e
Filip <filipf6@gmail.com>
2019-05-15 18:48:03 +0200

    Model evaluation

--- machine_learning_module/linear_regression/linear_regresssion.py
+++ machine_learning_module/linear_regression/linear_regresssion.py
@@ -4,8 +4,9 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 from common.common import all_models_variables, defender_specific_variables
-from tabulate import tabulate
+import copy
 
 # =============================================================================
 # get data from database
@@ -20,29 +21,21 @@ defender_samples_dataframe = pd.DataFrame(list(defender_samples_dataset))
 
 
 # =============================================================================
-# choose only needed columns
+# choose variables for regression, drop rows with missing values
 # 
 # =============================================================================
 defender_model_variables = all_models_variables + defender_specific_variables
 defender_model_variables.append('score')
-defender_samples_dataframe = defender_samples_dataframe[defender_model_variables]
+defender_samples_dataframe = defender_samples_dataframe[defender_model_variables].dropna()
 
 # =============================================================================
-# converts string values to floats
+# converts string values to floats, calculate statistical details of dataset
 # 
 # =============================================================================
 
 columns_to_convert = ['points_per_game','form','selected_by_percent','previous_fixtures_influence_2','previous_fixtures_creativity_2','previous_fixtures_threat_2','previous_fixtures_ict_index_2']
 defender_samples_dataframe[columns_to_convert] = defender_samples_dataframe[columns_to_convert].astype(float)
-
-# =============================================================================
-# fill missing data with mean
-# 
-# =============================================================================
-for var in defender_model_variables:
-    defender_samples_dataframe[var].fillna(defender_samples_dataframe[var].mean(), inplace=True)
-
-#print(defender_samples_dataframe.count())
+stats = defender_samples_dataframe.describe()
 
 # =============================================================================
 # divide data into dependent and undependent variables
@@ -57,7 +50,9 @@ defender_dependent_variable = defender_samples_dataframe["score"]
 # =============================================================================
 X_train, X_test, y_train, y_test = train_test_split(defender_undependent_variables, defender_dependent_variable, test_size=0.2, random_state=0)
 
-#train the algorithm
+# =============================================================================
+# train the algorithm
+# =============================================================================
 regressor = LinearRegression()  
 regressor.fit(X_train, y_train) 
 
@@ -65,22 +60,16 @@ regressor.fit(X_train, y_train)
 #coeff_df = pd.DataFrame(regressor.coef_, defender_undependent_variables.columns, columns=['Coefficient'])  
 #print(coeff_df) 
 
-#test the model
+# =============================================================================
+# test the model
+# =============================================================================
 y_pred = regressor.predict(X_test)
 
-predictions = X_test.iloc[:,:]
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred))) 
+print('R-squared: ', metrics.r2_score(y_test, y_pred))
 
-predictions['prediction'] = y_pred
-
-#a = np.c_[X_test, y_pred]
-
-
-#b = np.c_(X_test[:,:7], y_pred)
-
-#print(a[:,:7]+a[:,len(a)-2:len(a)-1])
-
-#print(tabulate(np.c_(a[:,:7]+a[len(a)-1]), headers=defender_model_variables[:7], tablefmt='orgtbl'))
-#print(tabulate(np.c_(X_test[:,:7], y_pred[y_pred.size-1:]), headers=defender_model_variables[:7], tablefmt='orgtbl'))
-
-
-
+predictions = copy.deepcopy(X_test)
+predictions['predict'] = y_pred
+predictions['real score'] = y_test
